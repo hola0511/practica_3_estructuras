8a9af84490d6b9a7615f52a658c1e7f49e04e0a6
hola0511 <espinosaariassamuel@gmail.com>
2025-04-24 23:50:53 -0500

    cambio en la barra de progreso

--- code/utils.py
+++ code/utils.py
@@ -12,13 +12,21 @@ def simulate_playback(playlist):
         song = playlist.show_current()
         print(f"▶️ Reproduciendo: {song}")
         for i in range(1, song.duration + 1):
-            print(f"⏳ Tiempo: {i}/{song.duration}s")
+            barra = generar_barra_progreso(i, song.duration)
+            print(f"{barra} {i}/{song.duration}s", end='\n')
             time.sleep(1)
+
 
         playlist.next_song()
         canciones_reproducidas += 1
 
     print("\n✅ Fin de la reproducción automática.")
+
+def generar_barra_progreso(actual, total, largo=20):
+    porcentaje = actual / total
+    llenos = int(largo * porcentaje)
+    vacíos = largo - llenos
+    return "█" * llenos + "░" * vacíos
 
 def shuffle_songs(songs):
     shuffled = songs[:]
@@ -28,6 +36,7 @@ def shuffle_songs(songs):
 def simulate_single_song(song):
     print(f"\n▶️ Reproduciendo: {song}")
     for i in range(1, song.duration + 1):
-        print(f"⏳ Tiempo: {i}/{song.duration}s")
+        barra = generar_barra_progreso(i, song.duration)
+        print(f"{barra} {i}/{song.duration}s", end='\n')
         time.sleep(1)
 
